d47337c9a558314bb9cdb4d669104d87b5867f24
Nadia <nbelghoz@gmail.com>
2021-04-09 14:10:53 -0400

    Started EventController and EventViews

---
+++ RedBadgeFinalProject.WebMVC/Controllers/EventController.cs
@@ -0,0 +1,38 @@
+﻿using RedBadgeFinalProject.Models.EventModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedBadgeFinalProject.WebMVC.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        [Authorize]
+        public ActionResult Index()
+        {
+            var model = new EventListItem[0];
+            return View(model);
+        }
+
+        //Get
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EventCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View(model);
+        }
+    }
+}
